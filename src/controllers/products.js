ea314c43ca91769bf7f427d0bd966d6cf2027f74
elissandrojunior <teofiloeli@gmail.com>
2022-12-22 20:44:24 -0300

    add delete method

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -46,8 +46,21 @@ async function put(req, res) {
     }) 
 }
 
+async function remove(req, res) {
+    const { id } = req.params
+
+    const remove = await ProductsModel.deleteOne({ _id: id })
+
+    console.log(remove.ok)
+
+    res.send({
+        message: 'success'
+    })
+}
+
 module.exports = {
     get,
     post,
-    put
+    put,
+    remove
 }
